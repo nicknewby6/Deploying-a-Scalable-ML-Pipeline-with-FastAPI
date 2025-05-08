06369f9c6395029f95c5e238ac05b6a2a621aace
Nick Newby <nicknewby6@gmail.com>
2025-05-07 18:42:32 -0600

    Implemented FastAPI functionality

--- local_api.py
+++ local_api.py
@@ -3,12 +3,13 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get(url='http://127.0.0.1:8000')
 
 # TODO: print the status code
-# print()
+print('Status Code:', r.status_code)
 # TODO: print the welcome message
-# print()
+msg = list(r.json().values())
+print('Welcome Message:', msg[0])
 
 
 
@@ -30,9 +31,10 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post(url = 'http://127.0.0.1:8000/data/', data=json.dumps(data))
 
 # TODO: print the status code
-# print()
+print('Status Code:', r.status_code)
 # TODO: print the result
-# print()
+msg = list(r.json().values())
+print('Result:', msg[0])
--- main.py
+++ main.py
@@ -26,21 +26,20 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+path = 'model/encoder.pkl' 
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = 'model/model.pkl'
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
+    return {'greeting':'Hello World!'}
 
 
 # TODO: create a POST on a different path that does model inference
@@ -65,10 +64,14 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
+        data,
+        cat_features,
+        training=False,
+        encoder=encoder
         # your code here
         # use data as data input
         # use training = False
         # do not need to pass lb as input
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
